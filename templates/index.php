6e6426887bab8fb40a28e0541ddf4f4c45a474d3
picpoint <picpoint@yandex.ru>
2023-05-29 16:54:13 +0300

    work with layout of index page

--- templates/index.php
+++ templates/index.php
@@ -5,11 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Todo List</title>
 </head>
 <body>
 
-    <h3>Шаблон PHP index!</h3>
+    <div class="indx__wrapper">
+        <div class="indx__header">
+            <div class="indx__headermenu">
+                <div class="indx__logoblock">
+                    <img src="../pict/logo.png" alt="logo">
+                </div>
+                <div class="indx__personal">
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
