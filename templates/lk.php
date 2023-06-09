e7943b52c3ed5d8da1f33d277b5ba003836d21d4
picpoint <picpoint@yandex.ru>
2023-06-09 08:52:29 +0300

    work with layout page lk

--- templates/lk.php
+++ templates/lk.php
@@ -5,11 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/css/style.css">
     <title>Личный кабинет</title>
 </head>
 <body>
 
-    <h3>Личный кабинет</h3>
+    <div class="lk__wrapper">
+        <div class="lk__panelleft">
+            <div class="lk__panellefthdr">
+                <img src="../public/pict/avatar.jpg" alt="avatar">
+                <span>WERY WERY LONG NAME</span>
+            </div>
+        </div>
+
+        <div class="lk__panelright"></div>
+    </div>
 
 </body>
 </html>
