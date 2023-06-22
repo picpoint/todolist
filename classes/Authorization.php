4339b09d6931b1ae7b44f8325bc87397bbdd1de1
picpoint <picpoint@yandex.ru>
2023-06-22 08:52:58 +0300

    work with auth users

--- classes/Authorization.php
+++ classes/Authorization.php
@@ -6,13 +6,45 @@ include_once __DIR__ . '/../autoload.php';
 class Authorization
 {
 
-    public function checkAuth($data) {
+    public function checkAuth($data)
+    {
         print_r($data);
 
-        $checkUser = new DB();
-        $res = $checkUser->getData($data);
+        echo "<br>";
+        echo "<br>";
 
-        print_r($res);
+        $checkUser = new DB();
+        $dataRaw = $checkUser->getData($data);
+
+        $res = $dataRaw[0];
+
+        echo "<pre>";
+        var_dump($res);
+        echo "</pre>";
+
+        if (!empty($res)) {
+            if ($res['email'] == $data['email']) {
+                echo "ACCESS";
+            } else {
+                echo "Неправильно введён логин или пароль...";
+            }
+        } else {
+            echo "Такого пользователя не существует...";
+        }
+
+
+//        if (!empty($res)) {
+//            foreach ($res as $key => $value) {
+//                if ($key == 'email' && $value == $data['email']) {
+//                    echo "ACCESS";
+//                } else {
+//                    echo "Неправильно введён логин или пароль...";
+//                }
+//            }
+//        } else {
+//            echo "Такого пользователя не существует...";
+//        }
+
 
 //        $checkUser->checkUserDate($data);
 
